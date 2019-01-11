bec42f7e8dda786bff2e0c4a6eb8e0762e1a7714
ZHI-XINHUA <1960881192@qq.com>
2019-01-11 18:00:02 +0800

    初始化项目

---
+++ src/main/java/zxh/DistributedSpringbootApplication.java
@@ -0,0 +1,14 @@
+package zxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSpringbootApplication.class, args);
+	}
+
+}
+
